bea38c695d4b068c9113567870d3b03f5b8ae408
alexfiftysix <amiller5656@gmail.com>
2018-05-25 13:42:57 +1000

    Suggestions on main page

--- back/Front/ng5/src/app/app.component.ts
+++ back/Front/ng5/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MatSnackBar} from '@angular/material';
 import {MatSnackBarConfig} from '@angular/material';
-import { AuthService} from '../services/auth.service';
+import {AuthService} from '../services/auth.service';
 import {Router} from '@angular/router';
-import {EventsDataService} from '../app/front-view/suggestions/events.service';
+import {EventsDataService} from './front-view/suggestions/events.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,19 +12,24 @@ import {EventsDataService} from '../app/front-view/suggestions/events.service';
 })
 
 export class AppComponent implements OnInit {
-  constructor(public snackBar: MatSnackBar,
-              public router: Router,
-              private eventService: EventsDataService) {
-  }
   public events;
   isSubmitActive = false;
   isLoginActive = false;
   isAccountChosen = false;
   accountType = '';
   title = 'app';
+
+  constructor(
+    public snackBar: MatSnackBar,
+    public router: Router,
+    private eventService: EventsDataService
+  ) {
+  }
+
   redirectToEventProfile(eventId) {
     this.router.navigate(['/eventDetails', eventId]);
   }
+
   getEvents(query: string) {
     return this.eventService.getEvents(query).subscribe(data => {
         this.events = data.events;
@@ -33,6 +39,7 @@ export class AppComponent implements OnInit {
         console.log('Events fetched completed');
       });
   }
+
   ngOnInit() {
     this.getEvents('');
   }
--- back/Front/ng5/src/app/front-view/front-view.component.ts
+++ back/Front/ng5/src/app/front-view/front-view.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {EventsDataService} from "./suggestions/events.service";
 
 @Component({
   selector: 'app-front-view',
@@ -6,11 +8,31 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./front-view.component.scss']
 })
 export class FrontViewComponent implements OnInit {
+  public events;
 
-  constructor() {
+  constructor(
+    public router: Router,
+    private eventService: EventsDataService
+  ) {
   }
 
   ngOnInit() {
+    this.events = null;
+    this.getEvents('');
+  }
+
+  redirectToEventProfile(eventId) {
+    this.router.navigate(['/eventDetails', eventId]);
+  }
+
+  getEvents(query: string) {
+    return this.eventService.getEvents(query).subscribe(data => {
+        this.events = data.events;
+        console.log(data);
+      }, error => console.log(error),
+      () => {
+        console.log('Events fetched completed');
+      });
   }
 
 }
